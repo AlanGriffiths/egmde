92525f1907fba8d32116d66051b4cf9bfa2f3fdd
Alan Griffiths <alan@octopull.co.uk>
2019-05-17 17:41:22 +0200

    Drop support for MirAL before 2.5

--- egwindowmanager.h
+++ egwindowmanager.h
@@ -19,13 +19,7 @@
 #ifndef EGMDE_EGWINDOWMANAGER_H
 #define EGMDE_EGWINDOWMANAGER_H
 
-#include <miral/version.h>
-
-#if MIRAL_VERSION >= MIR_VERSION_NUMBER(2, 5, 0)
-    #include <miral/minimal_window_manager.h>
-#else
-    #include <miral/canonical_window_manager.h>
-#endif
+#include <miral/minimal_window_manager.h>
 
 namespace egmde
 {
@@ -33,11 +27,7 @@ using namespace miral;
 class Wallpaper;
 
 class WindowManagerPolicy :
-#if MIRAL_VERSION >= MIR_VERSION_NUMBER(2, 5, 0)
     public MinimalWindowManager
-#else
-    public CanonicalWindowManagerPolicy
-#endif
 {
 public:
     WindowManagerPolicy(WindowManagerTools const& tools, Wallpaper const& wallpaper);
@@ -45,64 +35,11 @@ public:
     auto place_new_window(ApplicationInfo const& app_info, WindowSpecification const& request_parameters)
         -> WindowSpecification override;
 
-#if MIRAL_VERSION < MIR_VERSION_NUMBER(2, 5, 0)
-    // Switch apps  : Alt+Tab
-    // Switch window: Alt+`
-    // Close window : Alt-F4
-    bool handle_keyboard_event(MirKeyboardEvent const* event) override;
-
-    // Switch apps  : click on the corresponding window
-    // Switch window: click on the corresponding window
-    // Move window  : Alt-leftmousebutton drag
-    bool handle_pointer_event(MirPointerEvent const* event) override;
-
-    // Switch apps  : tap on the corresponding window
-    // Switch window: tap on the corresponding window
-    // Move window  : three finger drag
-    // Resize window: three finger pinch
-    bool handle_touch_event(MirTouchEvent const* event) override;
-
-    Rectangle confirm_placement_on_display(
-        WindowInfo const& window_info, MirWindowState new_state, Rectangle const& new_placement) override;
-
-    void handle_request_drag_and_drop(WindowInfo& window_info) override;
-
-    void handle_request_move(WindowInfo& window_info, MirInputEvent const* input_event) override;
-
-    void handle_request_resize(WindowInfo& window_info, MirInputEvent const* input_event, MirResizeEdge edge) override;
-#endif
-
 private:
     Wallpaper const* wallpaper;
 
-#if MIRAL_VERSION < MIR_VERSION_NUMBER(2, 5, 0)
-    // State held for move/resize gesture by pointer
-    enum PointerGesture {
-        pointer_gesture_none,
-        pointer_gesture_moving,
-        pointer_gesture_resizing
-    } pointer_gesture = pointer_gesture_none;
-    MirPointerButton pointer_gesture_button;
-    miral::Window pointer_gesture_window;
-    unsigned pointer_gesture_shift_keys = 0;
-    MirResizeEdge resize_edge = mir_resize_edge_none;
-    Point resize_top_left;
-    Size resize_size;
-
-    // State held for move/resize gesture by touch
-    int old_touch_pinch_top = 0;
-    int old_touch_pinch_left = 0;
-    int old_touch_pinch_width = 0;
-    int old_touch_pinch_height = 0;
-    bool pinching = false;
-
-    void end_touch_gesture();
-#endif
     void keep_size_within_limits(
         WindowInfo const& window_info, Displacement& delta, Width& new_width, Height& new_height) const;
-#if MIRAL_VERSION < MIR_VERSION_NUMBER(2, 5, 0)
-    bool begin_pointer_gesture(WindowInfo const& window_info, MirInputEvent const* input_event, PointerGesture gesture);
-#endif
 };
 }
 

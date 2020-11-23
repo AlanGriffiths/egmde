Pajn/Cascade

Add gtk-primary-selection

egmde.cpp
@@ -20,6 +20,7 @@
 #include "egwindowmanager.h"
 #include "eglauncher.h"
 #include "primary_selection.h"
+#include "gtk_primary_selection.h"
 
 #include <miral/append_event_filter.h>
 #include <miral/command_line_option.h>
@@ -114,8 +115,8 @@ int main(int argc, char const* argv[])
     runner.add_stop_callback([&] { launcher.stop(); });
 
     WaylandExtensions wayland_extensions;
-
     wayland_extensions.add_extension(egmde::primary_selection_extension());
+    wayland_extensions.add_extension(egmde::gtk_primary_selection_extension());
 
     return runner.run_with(
         {

AlanGriffiths/egmde

egmde.cpp
@@ -21,6 +21,7 @@
 #include "egshellcommands.h"
 #include "eglauncher.h"
 #include "primary_selection.h"
+#include "gtk_primary_selection.h"
 
 #include <miral/append_event_filter.h>
 #include <miral/command_line_option.h>
@@ -88,6 +89,7 @@ int main(int argc, char const* argv[])
     runner.add_stop_callback([&] { launcher.stop(); });
 
     extensions.add_extension(egmde::primary_selection_extension());
+    extensions.add_extension(egmde::gtk_primary_selection_extension());
 
     int no_of_workspaces = 1;
     auto const update_workspaces = [&](int option)